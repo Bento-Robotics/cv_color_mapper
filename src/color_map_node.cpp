0abc6b9cc3880fb0a3b416d25bd4f1b3fa9ca5e3
snaens <snaens@snaens.net>
2025-06-29 22:30:14 +0200

    initial working version

--- src/color_map_node.cpp
+++ src/color_map_node.cpp
@@ -1,59 +1,76 @@
 #include <memory>
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
-#include "cv_bridge/cv_bridge.h"
+#include "image_transport/image_transport.hpp"
+#include "cv_bridge/cv_bridge.hpp"
 #include "opencv2/opencv.hpp"
 
 class ColorMapNode : public rclcpp::Node
 {
-public:
-    ColorMapNode() : Node("color_map_node")
-    {
-        // Subscriber to the black and white image topic
-        image_subscriber_ = this->create_subscription<sensor_msgs::msg::Image>(
-            "input_image", 10, std::bind(&ColorMapNode::image_callback, this, std::placeholders::_1));
-
-        // Publisher for the colored image topic
-        image_publisher_ = this->create_publisher<sensor_msgs::msg::Image>("output_image", 10);
+  public:
+    ColorMapNode() : Node("color_map_node"){};
+    ~ColorMapNode() {
+      sub_->shutdown();
+      pub_->shutdown();
     }
 
-private:
-    void image_callback(const sensor_msgs::msg::Image::SharedPtr msg)
-    {
-        // Convert ROS image to OpenCV format
-        cv_bridge::CvImagePtr cv_ptr;
-        try
-        {
-            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_8UC1);
-        }
-        catch (cv_bridge::Exception& e)
-        {
-            RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
-            return;
-        }
-
-        // Apply OpenCV color map
-        cv::Mat colored_image;
-        cv::applyColorMap(cv_ptr->image, colored_image, cv::COLORMAP_JET); // You can change the colormap here
-
-        // Convert back to ROS image format
-        cv_bridge::CvImage output_msg;
-        output_msg.header = msg->header; // Use the same header as the input
-        output_msg.encoding = sensor_msgs::image_encodings::BGR8; // Change encoding to BGR
-        output_msg.image = colored_image;
-
-        // Publish the colored image
-        image_publisher_->publish(output_msg.toImageMsg());
+  public:
+    void InitializeImageTransport() {
+      it_ = std::make_shared<image_transport::ImageTransport>(
+          this->shared_from_this()); // https://answers.ros.org/question/353828/getting-a-nodesharedptr-from-this/
+      sub_ = std::make_shared<image_transport::Subscriber>(
+          it_->subscribe("input_image", 1,
+            std::bind(&ColorMapNode::image_callback, this,
+            std::placeholders::_1)));
+      pub_ = std::make_shared<image_transport::Publisher>(
+          it_->advertise("output_image", 1));
     }
 
-    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_subscriber_;
-    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_publisher_;
+
+    void image_callback (const sensor_msgs::msg::Image::ConstSharedPtr & msg) {
+      // Convert ROS image to OpenCV format
+      cv_bridge::CvImagePtr cv_ptr;
+      try
+      {
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_8UC1);
+      }
+      catch (cv_bridge::Exception& e)
+      {
+        auto logger = rclcpp::get_logger("color_map_node");
+        RCLCPP_ERROR(logger, "cv_bridge exception: %s", e.what());
+        return;
+      }
+
+      // Apply OpenCV color map
+      cv::Mat color_mapped_image;
+      cv::convertScaleAbs(cv_ptr->image, color_mapped_image, 40, -250); // alpha=contrast, beta=brightness
+      cv::applyColorMap(color_mapped_image, color_mapped_image, cv::COLORMAP_INFERNO);
+
+      // Convert back to ROS image format
+      sensor_msgs::msg::Image::SharedPtr output_msg = cv_bridge::CvImage(
+          msg->header, // Use the same header as the input
+          sensor_msgs::image_encodings::BGR8, // Change encoding to BGR
+          color_mapped_image
+          ).toImageMsg();
+
+      // Publish the resulting image
+      pub_->publish(output_msg);
+    }
+
+  private:
+    std::shared_ptr<image_transport::ImageTransport> it_;
+    std::shared_ptr<image_transport::Subscriber> sub_;
+    std::shared_ptr<image_transport::Publisher> pub_;
 };
 
-int main(int argc, char **argv)
-{
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<ColorMapNode>());
-    rclcpp::shutdown();
-    return 0;
+int main(int argc, char **argv) {
+  rclcpp::init(argc, argv);
+
+  auto node = std::make_shared<ColorMapNode>();
+  node->InitializeImageTransport();
+
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  rclcpp::spin_some(node); // better crash than segfault
+  return 0;
 }
